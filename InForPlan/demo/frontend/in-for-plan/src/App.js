8b87f4458b6435a9c94ee889a5e2eedf8a7afaea
yu-heejin <jkgt0521@naver.com>
2022-04-29 01:40:52 +0900

    commit

--- InForPlan/demo/frontend/in-for-plan/src/App.js
+++ InForPlan/demo/frontend/in-for-plan/src/App.js
@@ -1,18 +1,73 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { Component, } from 'react';
+import {useRef, useState, useEffect} from 'react';
+import styled from "styled-components";
 import './App.css';
 
-import TopMenuComponent from "./TopMenuComponent";
+const LoginApp = () => {
+    const userRef = useRef();
+    const errRef = useRef();
 
-function App() {
-  return (
-      <div className="App">
-          <div>
-              <TopMenuComponent>
-              </TopMenuComponent>
-          </div>
-      </div>
-  );
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [id, pw])
+
+    const submitHandle = async (e) => {
+        e.preventDefault();
+        console.log(id, pw);
+        setId('');
+        setPw('');
+        setSuccess(true);
+    }
+
+    return (
+        <>
+        <DivStyle>
+            <h2>Login</h2>
+            <form onSubmit={submitHandle}>
+                <p><input 
+                    type="text" 
+                    id="id" 
+                    placeholder="ID"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setId(e.target.value)}
+                    value={id}
+                    required></input></p>
+                <p><input type="password" 
+                    id="pw" 
+                    placeholder="Password"
+                    onChange={(e) => setPw(e.target.value)}
+                    value={pw}
+                    required></input></p>
+                <p><input type="submit" value="LOGIN"></input></p>
+            </form>
+
+            <p>
+                <a href="#">sign up</a>
+            </p>
+            
+        </DivStyle>   
+        </> 
+    );
+    
 }
 
-export default App;
+//style code list
+const DivStyle = styled.div`
+    width: 800px;
+    height: 500px;
+    background-color: white;
+    margin: 200px auto;
+    border-radius: 30px;
+`;
+
+export default LoginApp;
