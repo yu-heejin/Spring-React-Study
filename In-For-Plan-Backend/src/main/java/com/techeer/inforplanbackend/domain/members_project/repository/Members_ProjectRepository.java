3fcad11bcd4ba0b53c585531057b4971f370f4e4
hee <jkgt0521@naver.com>
2022-06-06 23:59:13 +0900

    react: modal

---
+++ In-For-Plan-Backend/src/main/java/com/techeer/inforplanbackend/domain/members_project/repository/Members_ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.techeer.inforplanbackend.domain.members_project.repository;
+
+import com.techeer.inforplanbackend.domain.members_project.entity.Members_Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Members_ProjectRepository extends JpaRepository <Members_Project,Long> {
+}
