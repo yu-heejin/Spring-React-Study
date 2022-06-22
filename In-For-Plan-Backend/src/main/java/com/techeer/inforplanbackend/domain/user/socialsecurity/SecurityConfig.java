96b5d9c09dca7e8887dc91c8c16ffa06da6257a0
hee <jkgt0521@naver.com>
2022-06-22 11:47:49 +0900

    spring security: google login

--- In-For-Plan-Backend/src/main/java/com/techeer/inforplanbackend/domain/user/socialsecurity/SecurityConfig.java
+++ In-For-Plan-Backend/src/main/java/com/techeer/inforplanbackend/domain/user/socialsecurity/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.techeer.inforplanbackend.domain.user.socialsecurity;
 
-import lombok.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,31 +11,29 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
-import static org.springframework.security.config.oauth2.client.CommonOAuth2Provider.GOOGLE;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests()
+        httpSecurity.authorizeRequests()
                 .antMatchers("/", "/oauth2/**", "/login/**", "/css/**", "/images/**", "/js/**", "/console/**", "/favicon.ico/**").permitAll()
-                .antMatchers("/google").hasAnyAuthority(GOOGLE.getRoleType())
+                .antMatchers("/google").hasAnyAuthority(CommonOAuth2Provider.GOOGLE.getRoleType())
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
                 .userInfoEndpoint().userService(new CustomOAuth2UserService())
                 .and()
-                .defaultSuccessUrl("/api/v1/loginSuccess")
-                .failureUrl("/api/v1/loginFailure")
+                .defaultSuccessUrl("/api/v1/login_success")
+                .failureUrl("/api/v1/login_fail")
                 .and()
                 .exceptionHandling()
-                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
+                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/api/v1/login"));
     }
 
-    private ClientRegistration getRegistration(OAuth2ClientProperties clientProperties, String client) {
-        if("google".equals(client)) {
+    public ClientRegistration getRegistration(OAuth2ClientProperties clientProperties, String client) {
+        if(client.equals("google")) {
             OAuth2ClientProperties.Registration registration = clientProperties.getRegistration().get("google");
 
             return CommonOAuth2Provider.GOOGLE.getBuilder(client)
