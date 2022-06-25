1194f8fb8ef8da22aa67f70099d5e2d7399cc20b
hee <jkgt0521@naver.com>
2022-06-25 15:45:11 +0900

    spring security: google login

--- Police-in-my-pocket-Backend--1-user-jpa-api/src/main/java/com/pocket/police/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pocket.police.repository;
-
-import com.pocket.police.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, String> {
-}
