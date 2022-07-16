eb05c6fd18301d8f52654687915e675619ebce01
hee <jkgt0521@naver.com>
2022-07-17 03:25:07 +0900

    spring security jwt

--- Police-in-my-pocket-Backend/src/main/java/com/pocket/police/global/security/JwtTokenProvider.java
+++ Police-in-my-pocket-Backend/src/main/java/com/pocket/police/global/security/JwtTokenProvider.java
@@ -1,4 +1,41 @@
 package com.pocket.police.global.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
 public class JwtTokenProvider {
+    private String secretKey = "policeInMyPocket";
+
+    //토큰 유효시간은 정하지 않음
+
+    private final UserDetailsService userDetailsService;
+
+    //객체 초기화, secretKey(디코딩용) Base64로 인코딩
+    @PostConstruct  //의존성 주입이 이루어진 후 초기화 수행
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    //JWT token 생성
+    public String CreateToken(String userId, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(userId);
+        claims.put("roles", roles);
+        Date date = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(date)   //토큰 발행 시간
+               // .setExpiration(new Date(date.getTime() + 유효시간 무한대 설정))
+                .compact();
+    }
 }
--- Police-in-my-pocket-Backend/src/main/java/com/pocket/police/global/security/WebSecurityConfig.java
+++ Police-in-my-pocket-Backend/src/main/java/com/pocket/police/global/security/WebSecurityConfig.java
@@ -8,11 +8,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-        http.authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll();
-        http
-                .httpBasic().disable()
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
     }
 }
